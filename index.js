040978478968044ae7ee7542df0bd4d11b43738b
vinoop vennekode <vinoopvennekode@gmail.com>
2023-03-27 10:12:50 +0530

    edit

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin:*");
+  res.setHeader("Access-Control-Allow-Origin","https://medicarebooking.online");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
