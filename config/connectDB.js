51cd69835b35f776ec10fadb15808082e7bc4549
vinoop vennekode <vinoopvennekode@gmail.com>
2023-03-27 10:25:11 +0530

    edit in mongo db config

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
+  mongoose.set('strictQuery',false);
   mongoose.connect(process.env.MONGODB_CONNECT).then(() => {
     // eslint-disable-next-line no-console
     console.log('mongdb connected');
